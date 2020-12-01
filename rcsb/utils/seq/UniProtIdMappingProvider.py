9f9404e13b483bb01472963427cf7763462ce71f
jwest <john.westbrook@rcsb.org>
2020-11-30 20:02:54 -0500

    V0.47 add UniProtIdMappingProvider() and tests.

---
+++ rcsb/utils/seq/UniProtIdMappingProvider.py
@@ -0,0 +1,160 @@
+##
+# File:    UniProtIdMappingProvider.py
+# Date:    30-Nov-2020
+#
+##
+
+import logging
+import os
+import time
+
+from rcsb.utils.io.FileUtil import FileUtil
+from rcsb.utils.io.IoUtil import IoUtil
+from rcsb.utils.io.MarshalUtil import MarshalUtil
+
+logger = logging.getLogger(__name__)
+
+
+class UniProtIdMappingProvider(object):
+    """Manage index of UniProt identifier mappings."""
+
+    def __init__(self, **kwargs):
+        urlTarget = kwargs.get("urlTarget", "ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/idmapping.dat.gz")
+        cachePath = kwargs.get("cachePath", ".")
+        dirPath = os.path.join(cachePath, "uniprot-id-mapping")
+        useCache = kwargs.get("useCache", True)
+        mapNameL = kwargs.get("mapNames", ["NCBI-taxon"])
+        fmt = kwargs.get("fmt", "pickle")
+        maxLimit = kwargs.get("maxLimit", None)
+        #
+        self.__mapRecordD = {
+            "UniProtKB-AC": 1,
+            "UniProtKB-ID": 2,
+            "GeneID": 3,
+            "RefSeq": 4,
+            "GI": 5,
+            "PDB": 6,
+            "GO": 7,
+            "UniRef100": 8,
+            "UniRef90": 9,
+            "UniRef50": 10,
+            "UniParc": 11,
+            "PIR": 12,
+            "NCBI-taxon": 13,
+            "MIM": 14,
+            "UniGene": 15,
+            "PubMed": 16,
+            "EMBL": 17,
+            "EMBL-CDS": 18,
+            "Ensembl": 19,
+            "Ensembl_TRS": 20,
+            "Ensembl_PRO": 21,
+            "Additional PubMed": 22,
+        }
+        self.__mU = MarshalUtil(workPath=dirPath)
+        self.__nameL, self.__mapD = self.__rebuildCache(urlTarget, mapNameL, dirPath, fmt=fmt, useCache=useCache, maxLimit=maxLimit)
+        self.__cacheFieldD = {k: ii for ii, k in enumerate(self.__nameL)}
+
+    def getMappedId(self, unpId, mapName="NCBI-taxon"):
+        mId = None
+        try:
+            #
+            iRec = self.__cacheFieldD[mapName]
+            mId = self.__mapD[unpId][iRec]
+        except Exception as e:
+            logger.exception("Failing %r with %s", self.__cacheFieldD, str(e))
+        return mId
+
+    def testCache(self):
+        logger.info("Length UniProt mapping for %r %d", self.__nameL, len(self.__mapD))
+        return len(self.__mapD) > 1000
+
+    def __rebuildCache(self, targetUrl, mapNameL, outDirPath, fmt="pickle", useCache=True, maxLimit=None):
+        """Fetch the UniProt selected id mapping resource file and extract
+        UniProt Acc to  'mapIndex' mapping. Serialize the mapping as required.
+
+        Args:
+            mapIndex (int): index in the tab delimited mapping file (below)
+            outDirPath (str): directory path for raw and processed mapping files
+            mapFileName (str): file name for the target mapping file
+            fmt (str, optional): output format (pickle|json) . Defaults to "pickle".
+            useCache (bool, optional): use cached files. Defaults to True.
+            maxLimit (int, optional): maximum number of records to process (default=None)
+
+        Returns:
+            dict: od[uniprotId] = mapped value
+
+                idmapping_selected.tab
+
+                1. UniProtKB-AC
+                2. UniProtKB-ID
+                3. GeneID (EntrezGene)
+                4. RefSeq
+                5. GI
+                6. PDB
+                7. GO
+                8. UniRef100
+                9. UniRef90
+                10. UniRef50
+                11. UniParc
+                12. PIR
+                13. NCBI-taxon
+                14. MIM
+                15. UniGene
+                16. PubMed
+                17. EMBL
+                18. EMBL-CDS
+                19. Ensembl
+                20. Ensembl_TRS
+                21. Ensembl_PRO
+                22. Additional PubMed
+
+        """
+        startTime = time.time()
+        nL = mapNameL
+        oD = {}
+        try:
+            fExt = "pic" if fmt == "pickle" else "json"
+            mapFileName = "uniprot-id-map." + fExt
+            idMapPath = os.path.join(outDirPath, mapFileName)
+            mU = MarshalUtil()
+            if useCache and mU.exists(idMapPath):
+                logger.info("Using cached file %r", idMapPath)
+                tD = mU.doImport(idMapPath, fmt=fmt)
+                nL = set(tD["idNameList"])
+                oD = tD["uniprotMapD"]
+                ok = True
+            else:
+                fileU = FileUtil()
+                targetUrl = "ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/idmapping_selected.tab.gz"
+                idPath = os.path.join(outDirPath, fileU.getFileName(targetUrl))
+                if not fileU.exists(idPath):
+                    logger.info("Fetch selected idmapping data from %r in %r", targetUrl, outDirPath)
+                    ok = fileU.get(targetUrl, idPath)
+                    if not ok:
+                        logger.error("Failed to downlowd %r", targetUrl)
+                        return oD
+                else:
+                    logger.info("Using cached mapping file %r", idPath)
+                # ---
+                ioU = IoUtil()
+                iCount = 0
+                for row in ioU.deserializeCsvIter(idPath, delimiter="\t", rowFormat="list", encodingErrors="ignore"):
+                    tL = []
+                    for mapName in mapNameL:
+                        mapRecord = self.__mapRecordD[mapName]
+                        tL.append(str(row[mapRecord - 1]).strip())
+                    if tL:
+                        oD[row[0]] = tL
+                        iCount += 1
+                        if iCount % 50000000 == 0:
+                            logger.info("Processing %d", iCount)
+                    if maxLimit and iCount > maxLimit:
+                        break
+                    #
+                ok = mU.doExport(idMapPath, {"idNameList": mapNameL, "uniprotMapD": oD}, fmt=fmt)
+            logger.info("Completed reload (%r) at %s (%.4f seconds)", ok, time.strftime("%Y %m %d %H:%M:%S", time.localtime()), time.time() - startTime)
+        except Exception as e:
+            logger.exception("Failing with %s", str(e))
+        #
+        return nL, oD
---
+++ rcsb/utils/tests-seq/testUniprotIdMappingProvider.py
@@ -0,0 +1,71 @@
+##
+# File:    UniProtIdMappingProviderProviderTests.py
+# Author:  J. Westbrook
+# Date:    30-Nov-2020
+#
+# Update:
+#
+#
+##
+"""
+Tests utilities to manage access to UniProtIdMappingProvider.
+
+"""
+
+__docformat__ = "restructuredtext en"
+__author__ = "John Westbrook"
+__email__ = "jwest@rcsb.rutgers.edu"
+__license__ = "Apache 2.0"
+
+import logging
+import os
+import platform
+import resource
+import time
+import unittest
+
+from rcsb.utils.seq.UniProtIdMappingProvider import UniProtIdMappingProvider
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+TOPDIR = os.path.dirname(os.path.dirname(HERE))
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s]-%(module)s.%(funcName)s: %(message)s")
+logger = logging.getLogger()
+
+
+class UniProtIdMappingProviderTests(unittest.TestCase):
+    skipFull = True
+
+    def setUp(self):
+        self.__dirPath = os.path.join(os.path.dirname(TOPDIR), "rcsb", "mock-data")
+        self.__cachePath = os.path.join(HERE, "test-output")
+        self.__startTime = time.time()
+        logger.info("Starting %s at %s", self.id(), time.strftime("%Y %m %d %H:%M:%S", time.localtime()))
+
+    def tearDown(self):
+        unitS = "MB" if platform.system() == "Darwin" else "GB"
+        rusageMax = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+        logger.info("Maximum resident memory size %.4f %s", rusageMax / 10 ** 6, unitS)
+        endTime = time.time()
+        logger.info("Completed %s at %s (%.4f seconds)", self.id(), time.strftime("%Y %m %d %H:%M:%S", time.localtime()), endTime - self.__startTime)
+
+    @unittest.skipIf(skipFull, "Very long test")
+    def testUniProtIdMappingProviderCache(self):
+        # umP = UniProtIdMappingProvider(cachePath=self.__cachePath, useCache=True, maxLimit=100)
+        umP = UniProtIdMappingProvider(cachePath=self.__cachePath, useCache=True)
+        logger.info("Completed %s at %s (%.4f seconds)", self.id(), time.strftime("%Y %m %d %H:%M:%S", time.localtime()), time.time() - self.__startTime)
+        #
+        taxId = umP.getMappedId("Q6GZX0", mapName="NCBI-taxon")
+        logger.info("TaxId %r", taxId)
+        self.assertEqual(taxId, "654924")
+
+
+def uniProtIdMappingProviderCacheSuite():
+    suiteSelect = unittest.TestSuite()
+    suiteSelect.addTest(UniProtIdMappingProviderTests("testUniProtIdMappingProviderCache"))
+    return suiteSelect
+
+
+if __name__ == "__main__":
+    mySuite = uniProtIdMappingProviderCacheSuite()
+    unittest.TextTestRunner(verbosity=2).run(mySuite)
